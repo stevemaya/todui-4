a54aac8dc595620a877339910ff5a53e2fb569dd
Steve Maya <steve.maya@codeimmersives.com>
2020-11-22 20:34:39 -0600

    commit

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const data = require('./data.js')
 
 
 const COMPLETE_MARK = '✅';
-const INCOMPLETE_MARK = '✖';
+const INCOMPLETE_MARK = '❌';
 
 let todos = data.todos;
 const interface = readline.createInterface({
@@ -13,47 +13,29 @@ const interface = readline.createInterface({
 
 const menu = `
 Your options are:
-
 1. Add a todo.
 2. Remove a todo.
 3. Remove all completed todos.
 4. Toggle a todo's completion status.
 5. Toggle a todo's priority.
 6. Quit.
-
 `
 
-const displayMenu = function() {
+const displayMenu = () => {
   interface.question(menu, handleMenu);
 }
 
-const displayTodos = function() {
+const displayTodos = () => {
   console.clear();
   console.log('\nHere are your current todos:\n')
   for (let i = 0; i < todos.length; i++) {
     const todo = todos[i];
     const num = i + 1;
-    console.log(num + '. ' + todo.text);
+    console.log(num + '. ' + todo.text + ' priority: ' + todo.priority + ' - ' + (todo.isComplete ? COMPLETE_MARK : INCOMPLETE_MARK));
   }
 }
 
-// or, without intermediate variables:
-const displayTodosAlt1 = function() {
-  console.log('\nHere are your current todos:\n')
-  for (let i = 0; i < todos.length; i++) {
-    console.log(i + 1 + '. ' + todos[i].text);
-  }
-}
-
-// or, with interpolation:
-const displayTodosAlt2 = function() {
-  console.log('\nHere are your current todos:\n')
-  for (let i = 0; i < todos.length; i++) {
-    console.log(`${i + 1}. ${todos[i].text}`);
-  }
-}
-
-const add = function(answer) {
+const add = (answer) => {
   const todo = {
     text: answer,
     priority: 2,
@@ -65,30 +47,80 @@ const add = function(answer) {
   displayMenu();
 }
 
-const remove = function(num) {
+const remove = (num) => {
   todos.splice(num - 1, 1);
   displayTodos();
   displayMenu();
 }
 
-const handleMenu = function(cmd) {
-  if (cmd === '1') {
-    console.clear();
-    interface.question('\nWhat should go on your list?\n\n', add)
-  } else if (cmd === '2') {
-    displayTodos();
-    interface.question('\nType a number to pick a todo to remove: ', remove)
-  } else if (cmd === '3') {
-    removeCompletedTodos();
-  } else if (cmd === '4') {
-    displayTodos();
-    interface.question('\nPlease pick a todo to check complete or incomlete: ', toggleComplete)
-  } else if (cmd === '5') {
-    displayTodos();
-    interface.question('\nPlease pick a todo to toggle its priority: ', togglePriority)
+const toggleComplete = (num) => {
+  // todos[num - 1].isComplete = (todos[num - 1].isComplete) ? false : true;
+  const theTodo = todos[num - 1]
+  if (theTodo.isComplete) {
+    theTodo.isComplete = false
   } else {
-    console.log('Quitting!');
-    interface.close();
+    theTodo.isComplete = true
+  }
+  displayTodos();
+  displayMenu();
+}
+
+
+
+const togglePriority = (num) => {
+  const todo = todos[num - 1];
+  // if(todo.priority === 1){
+  //   todos.priority = 2
+  // } else (todo.priority === 2)
+  //   todos.priority = 1
+
+  todos[num - 1].priority = (todos[num - 1].priority == 1) ? 2 : 1;
+
+  displayTodos();
+  displayMenu();
+
+}
+
+const removeCompletedTodos = () => {
+  todos = todos.filter(function (todo) {
+    return todo.isComplete === false;
+  })
+
+  displayTodos();
+  displayMenu();
+
+}
+
+
+const handleMenu = (cmd) => {
+  switch (cmd) {
+    case '1':
+      console.clear();
+      interface.question('\nWhat should go on your list?\n\n', add)
+      break;
+
+    case '2':
+      displayTodos();
+      interface.question('\nType a number to pick a todo to remove: ', remove)
+      break;
+
+    case '3':
+      removeCompletedTodos();
+      break;
+
+    case '4':
+      displayTodos();
+      interface.question('\nPlease pick a todo to check complete or incomplete: ', toggleComplete)
+      break;
+
+    case '5':
+      displayTodos();
+      interface.question('\nPlease pick a todo to toggle its priority: ', togglePriority)
+      break;
+
+    default:
+      console.log('Quitting!');
+      interface.close();
   }
 }
 
